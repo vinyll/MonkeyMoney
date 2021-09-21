e4dfc47fb932e5fbc91476756e2f0081bcd482b9
Vincent Agnano <vinyll@protonmail.com>
2021-09-21 11:05:11 +0200

    updated fresh new logo

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <van-row>
       <van-col span="16" offset="4">
-        <img src="~/static/monkeymoney.svg" width="100%" />
+        <img src="~/static/monkeymoney-full.svg" width="100%" />
         <van-button round type="primary" block to="/signup" v-show="!this.user">Sign up</van-button>
         <van-button round block v-show="!this.user" to="/login">Log in</van-button>
         <van-button round block v-show="this.user" @click="$store.commit('logout')">Log out</van-button>
